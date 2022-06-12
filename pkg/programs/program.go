4d33b863e40e7cca3b729463cda89c3cf876744b
antoniovini <antoniovinicius1801@gmail.com>
2022-06-12 17:59:02 -0300

    removed role based programs to solve a bug

--- pkg/programs/program.go
+++ pkg/programs/program.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 	"otontech/console-api/models"
 	"otontech/console-api/utils/middlewares"
-	"sort"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -25,10 +24,36 @@ func RegisterRoutes(r *gin.Engine, db *gorm.DB) {
 		middlewares.JwtAuthMiddleware(false),
 		h.GetProgram,
 	)
+	routes.GET(
+		"/",
+		middlewares.JwtAuthMiddleware(false),
+		h.GetPrograms,
+	)
+}
+
+func (h ProgramHandler) GetPrograms(c *gin.Context) {
+	// userContext, exists := c.Get("user")
+	var programs []models.Program
+
+	h.DB.Model(&models.Program{}).Preload("Roles").Find(&programs)
+
+	// var userPrograms []models.Program
+	// var userRoles []string
+
+	// if exists {
+	// 	u := userContext.(models.User)
+	// 	for i := range u.Roles {
+	// 		userRoles = append(userRoles, u.Roles[i].Name)
+	// 	}
+	// } else {
+	// 	userRoles = append(userRoles, "default")
+	// }
+
+	c.JSON(http.StatusOK, gin.H{"data": programs})
 }
 
 func (h ProgramHandler) GetProgram(c *gin.Context) {
-	userContext, exists := c.Get("user")
+	// userContext, exists := c.Get("user")
 	var program models.Program
 
 	if err := h.DB.Preload("Roles").Where("name = ?", c.Param("name")).First(&program).Error; err != nil {
@@ -36,27 +61,28 @@ func (h ProgramHandler) GetProgram(c *gin.Context) {
 		return
 	}
 
-	var userRoles []string
+	// var userRoles []string
 
-	if exists {
-		u := userContext.(models.User)
-		for i := range u.Roles {
-			userRoles = append(userRoles, u.Roles[i].Name)
-		}
-	} else {
-		userRoles = append(userRoles, "default")
-	}
+	// if exists {
+	// 	u := userContext.(models.User)
+	// 	for i := range u.Roles {
+	// 		userRoles = append(userRoles, u.Roles[i].Name)
+	// 	}
+	// } else {
+	// 	userRoles = append(userRoles, "default")
+	// }
 
-	for i := range program.Roles {
-		idx := sort.Search(len(userRoles), func(j int) bool {
-			return userRoles[j] == program.Roles[i].Name
-		})
+	// for i := range program.Roles {
+	// 	idx := sort.Search(len(userRoles), func(j int) bool {
+	// 		return userRoles[j] == program.Roles[i].Name
+	// 	})
 
-		if !(idx < len(userRoles) && userRoles[idx] == program.Roles[i].Name) {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Program not found!"})
-			return
-		}
-	}
+	// 	if !(idx < len(userRoles) && userRoles[idx] == program.Roles[i].Name) {
+	// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Program not found!"})
+	// 		return
+	// 	}
+	// }
 
+	// TODO - check user programs
 	c.JSON(http.StatusOK, gin.H{"data": program})
 }
